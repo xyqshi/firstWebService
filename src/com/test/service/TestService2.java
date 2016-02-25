47c3106cfea397e6d87467604f2ed90deaca3cf9
Shi Qiu <xyqshi@gmail.com>
2016-02-24 18:55:01 -0800

    add new method which takes paramters from http request

--- src/com/test/service/TestService2.java
+++ src/com/test/service/TestService2.java
@@ -2,10 +2,14 @@ package com.test.service;
 
 import java.util.List;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/TestService")
 public class TestService2 {
@@ -18,4 +22,15 @@ public class TestService2 {
    public List<User> getUsers(){
       return userDao.getAllUsers();
    }	
+   
+ //@GET
+   //@Path("/{parameter}")
+   public Response responseMsg( @PathParam("parameter") String parameter,
+           @DefaultValue("Nothing to say") @QueryParam("value") String value) {
+
+       String output = "Hello from: " + parameter + " : " + value;
+
+       return Response.status(200).entity(output).build();
+
+   }
 }
